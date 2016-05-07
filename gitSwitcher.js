086665672e77fa1f2691d03fa2f878c4f28bafaa
SHOTA GUTERRES <SHOTA@Peripatetica.domain>
2016-05-06 22:46:08 -0700

    homework time

---
+++ gitSwitcher.js
@@ -0,0 +1,29 @@
+	
+var theWeek = 'Tuesday';
+
+switch(theWeek) {
+	case 'Monday':
+		console.log('Fu**ing Mondays');
+		break;
+	case 'Tuesday':
+		console.log('Toking Tuesdays');
+		break;
+	case 'Wednesday':
+		console.log('Wanking Wednesdays');
+		break;
+	case 'Thursday':
+		console.log('Thirsty Thursdays');
+		break;
+	case 'Friday':
+		console.log('Freaky Fridays');
+		break;
+	case 'Saturday':
+		console.log('Sipping Down Saturdays');
+		break;
+	case 'Sunday':
+		console.log('Sitting back Sundays');
+		break;
+	default: 
+		console.log("pick a day of the week!");
+
+}
